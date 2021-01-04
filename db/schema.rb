989e422ffe1cc486ccb7f9a41e14b1e5f5dea0c1
arthurgeay <arthurgeay.contact@gmail.com>
2021-01-04 17:01:16 +0100

    Add listen gem dependency

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_01_04_090042) do
-=======
 ActiveRecord::Schema.define(version: 2021_01_04_133211) do
->>>>>>> 240d63dde7728acaa44ba7194530045c09c8a98c
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-=======
   create_table "project_users", force: :cascade do |t|
-    t.bigint "users_id"
+    t.bigint "user_id"
     t.string "role"
-    t.bigint "projects_id"
+    t.bigint "project_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["projects_id"], name: "index_project_users_on_projects_id"
-    t.index ["users_id"], name: "index_project_users_on_users_id"
+    t.index ["project_id"], name: "index_project_users_on_project_id"
+    t.index ["user_id"], name: "index_project_users_on_user_id"
   end
 
->>>>>>> 240d63dde7728acaa44ba7194530045c09c8a98c
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -40,8 +33,6 @@ ActiveRecord::Schema.define(version: 2021_01_04_133211) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "task_lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -64,7 +55,6 @@ ActiveRecord::Schema.define(version: 2021_01_04_133211) do
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
->>>>>>> 240d63dde7728acaa44ba7194530045c09c8a98c
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
