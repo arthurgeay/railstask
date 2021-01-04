07a220e7b8c70e5644f736cd965922273668a9b0
Auriane Labille <auriane.labille@gmail.com>
2021-01-04 13:53:46 +0100

    Update Gemfile & schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_01_04_090042) do
-ActiveRecord::Schema.define(version: 2021_01_04_085457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,12 +34,6 @@ ActiveRecord::Schema.define(version: 2021_01_04_085457) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "color"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
