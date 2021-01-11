9fd62daafda529fb1922fae2e13fa94d94a18bbb
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-01-11 09:52:59 +0100

    Delete project in cascade

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
     validates :name, :description, :color, presence: true
-    has_many :task_lists
-    has_many :project_users
+    has_many :task_lists, dependent: :destroy
+    has_many :project_users, dependent: :destroy
     has_many :users, through: :project_users
     accepts_nested_attributes_for :project_users
 end
